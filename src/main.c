9c7cf918a63e215f267b19150ddf4756b5d14ffb
sheph2mj <sheph2mj@cmich.edu>
2016-11-22 11:08:58 -0500

    still trying to get split into array of strings by space to work
    :neckbeard:

--- src/main.c
+++ src/main.c
@@ -10,24 +10,39 @@ const char *special_words[] = {
     "not",
 };
 
-char *split_tokens_by_space(char tokens[]){
-    char *split_tokens = malloc(10 * sizeof(char));
-    char *
-    int i;
+void print_a(char *arr[], int len){
+    for(int i = 0; i < len; i++)
+        printf("%s\n", arr[i]);
+}
 
-    for(i = 0; i < 10; i++)
-        printf("split_tokens[%d]: %c\n", i, split_tokens[i]);
+
+char *split_tokens_by_space(char line[]){
+    char *split_tokens = malloc(10 * sizeof(char));
+    char *char_ptr;
+    char_ptr = strtok(line, " ");
+    int i = 0;
+    while(char_ptr != NULL){
+        printf("%s\n", char_ptr);
+        char_ptr = strtok(NULL, " ");
+        split_tokens[i] = *(char*)malloc(sizeof(char));
+        strcpy(split_tokens[i], char_ptr);
+        ++i;
+    }
     return split_tokens;
 }
 
 char *file_to_list_of_tokens(FILE* file){
     char *list_of_tokens;
     int line_limit = 1000;
-    char buffer[line_limit];
+    char line[line_limit];
+
     while(!feof(file)){
-        fgets(buffer, line_limit, file);
-        char *split_tokens = split_tokens_by_space(buffer);
+
+        fgets(line, line_limit, file);
+        char *split_tokens = split_tokens_by_space(line);
+        print_a(&split_tokens, 4); 
     }
+
     return list_of_tokens;
 }
 

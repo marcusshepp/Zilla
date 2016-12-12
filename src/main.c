880f4d692558d915dbf94486e8f557ed55f9c24c
sheph2mj <sheph2mj@cmich.edu>
2016-12-12 10:22:11 -0500

    comments

--- src/main.c
+++ src/main.c
@@ -17,28 +17,50 @@ void print_a(char *arr[], int len){
 
 
 char *split_tokens_by_space(char line[]){
-    char *split_tokens = malloc(10 * sizeof(char));
+    /* IN: a single string line from the file
+     * "foo + bar"
+     * OUT: an array of each token from the line
+     * split by spaces.
+     * ["foo", "+", "bar"]
+     * */
+    // create an array to hold all of the strings from the line
+    char* split_tokens = malloc(10 * sizeof(char));
+    // `char_ptr` will be a pointer to the current token
     char *char_ptr;
     char_ptr = strtok(line, " ");
     int i = 0;
+
     while(char_ptr != NULL){
+
         printf("%s\n", char_ptr);
+        // point the pointer at the next token
         char_ptr = strtok(NULL, " ");
+        // allocate a space in mem for the current token
+        // within the holder array
         split_tokens[i] = *(char*)malloc(sizeof(char));
+        // copy the string over to the holder array
         strcpy(split_tokens[i], char_ptr);
         ++i;
+
     }
+    // return the holder array
     return split_tokens;
 }
 
 char *file_to_list_of_tokens(FILE* file){
+    /* IN: file pointer
+     * OUT: list of tokens seperated by space
+     * ["foo", "+", "bar"]
+     * */
     char *list_of_tokens;
     int line_limit = 1000;
     char line[line_limit];
 
     while(!feof(file)){
-
+        // for each line 
+        // put line contents into buffer
         fgets(line, line_limit, file);
+        // pass buffer into `split by tokens` function
         char *split_tokens = split_tokens_by_space(line);
         print_a(&split_tokens, 4); 
     }
